17f9628d4b9e215b40e4988af5b71f93e7bc0999
Franklin Jiang <Chief@Jiangyj.Tech>
2023-09-15 22:57:20 +0800

    1

---
+++ Vision/1_ConnectedLabel.py
@@ -0,0 +1,90 @@
+# %%
+from tool import *
+
+# 读取图像（第二个参数0表示以灰度图像方式读取图像）
+img = imread("1_counting_objects_rgb.png")
+
+# %% 将彩图转化为 BW 图
+img = im_replace(img, (0, 255, 255), (255, 255, 255))
+img = im_replace(img, (255, 255, 255), (0, 0, 0), not_equal=True)
+
+# %%
+# 初始化并查集
+ufs = UnionFind()
+# 将三通道 BGR 转成单通道
+img = img[:, :, 0]
+# 获取图像的尺寸
+M, N = img.shape
+I = np.zeros_like(img, dtype=np.int32)
+
+label = 1
+
+# 如果左上角点为黑
+if img[0, 0] == 0:
+    I[0, 0] = label
+    ufs.add(label)
+    label += 1
+
+# 第一遍
+for i in range(M):
+    for j in range(N):
+        # 如果该点为黑
+        if img[i, j] == 0:
+            # 如果是第一行
+            if i == 0 and j != 0:
+                # 取左元素的值
+                val = I[i, j - 1]
+
+                # 如果该值为标记值（非零值）
+                if val != 0:
+                    I[i, j] = val
+
+                # 如果该值为零值
+                else:
+                    I[i, j] = label
+                    ufs.add(label)
+                    label += 1
+
+            # 如果是第一列
+            elif i != 0 and j == 0:
+                # 取上元素的值
+                val = I[i - 1, j]
+
+                # 如果该值为标记值（非零值）
+                if val != 0:
+                    I[i, j] = val
+
+                # 如果该值为零值
+                else:
+                    I[i, j] = label
+                    ufs.add(label)
+                    label += 1
+
+            # 对于中间的常规元素
+            else:
+                # 取左元素和上元素的值
+                a = I[i, j - 1]
+                b = I[i - 1, j]
+
+                I[i, j] = a or b
+                if I[i, j] == 0:
+                    I[i, j] = label
+                    ufs.add(label)
+                    label += 1
+
+                if a and b:
+                    ufs.merge(a, b)
+
+print(I)
+
+# 第二遍
+for i in range(M):
+    for j in range(N):
+        # 如果该点为黑
+        if img[i, j] == 0:
+            # print(I[i, j], ufs.find(I[i, j]))
+            I[i, j] = ufs.find(I[i, j])
+
+print(I)
+
+print("Number of objects:", len(set(ufs.parent.values())))
--- Vision/tool.py
+++ Vision/tool.py
@@ -1,23 +1,31 @@
 import cv2, copy, sys
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
 
 # 将 NumPy 的默认截断改为系统允许的最大大小
 np.set_printoptions(threshold=sys.maxsize)
 
 
-def imshow(img, OpenCV=False):
+def imshow(img):
     """展示图像"""
     # 如果展示的是 OpenCV 的图像
-    if OpenCV:
+    if type(img) == type(np.array([])):
         # 则要将 OpenCV 的 BGR 图像转换为 RGB 图像
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     plt.imshow(img)
     plt.axis("off")  # 去除坐标轴
 
 
-def im_replace(img: np.array, _from, _to, not_equal=False):
-    """_from 填诸如 [255,255,0]；_to 填诸如 [0,0,0]；not_equal 是不等条件，默认为 False 表等于条件，若为 True，则替换掉满足不等条件的元素"""
+def imread(filename: str):
+    """用 OpenCV（BGR）读取图片文件。资源放置在 Src 文件夹下，传入文件名即可。"""
+    img = cv2.imread(f"Src/{filename}")
+    imshow(img)
+    return img
+
+
+def im_replace(img: np.array, _from: tuple, _to: tuple, not_equal=False):
+    """_from 和 _to 传入 RGB 元素如 (255, 255, 0), (0, 0, 0)；not_equal 是不等条件，默认为 False（即等于），若为 True，则替换掉满足不等条件的元素。OpenCV（BGR）的通道差异已经封装处理，按 RGB 传入即可。"""
     # 获取深层拷贝
     img = copy.deepcopy(img)
     # 重塑数组的 shape
@@ -26,12 +34,57 @@ def im_replace(img: np.array, _from, _to, not_equal=False):
     # 进行替换
     if not_equal:
         # 如果是不相等的话，只要有一个元素不相等成立即可
-        img_flatten[np.any(img_flatten != _from, axis=1)] = _to
+        img_flatten[np.any(img_flatten != _from[::-1], axis=1)] = _to[::-1]
     else:
         # 如果是相等的话，需要三个元素全都相等成立才行
-        img_flatten[np.all(img_flatten == _from, axis=1)] = _to
+        img_flatten[np.all(img_flatten == _from[::-1], axis=1)] = _to[::-1]
     # 恢复数组的 shape
     img = img_flatten.reshape((shape[0], shape[1], shape[2]))
     # print(img_flatten)
     imshow(img)
     return img
+
+
+class UnionFind:
+    """采用字典来维护 Parent"""
+
+    def __init__(self, element_num=None):
+        self.parent = {}
+        if element_num is not None:
+            for i in range(element_num):
+                self.add(i)
+
+    def add(self, x):
+        """工具函数，不单独使用"""
+        # 如果已经存在则跳过
+        if x in self.parent:
+            return
+        self.parent[x] = x
+
+    def merge(self, x, y):
+        if x not in self.parent:
+            self.add(x)
+        if y not in self.parent:
+            self.add(y)
+        # 查找到两个元素的树根
+        x = self.find(x)
+        y = self.find(y)
+        # 如果相等，说明属于同一个集合
+        if x == y:
+            return
+        elif x < y:
+            self.parent[y] = x
+        else:
+            self.parent[x] = y
+
+    def find(self, x):
+        # 若 Parent 为自身，则已经找到 root
+        if self.parent[x] == x:
+            return x
+        # 否则不断向上找并更新 Parent
+        self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    # 判断是否属于同一个集合
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
